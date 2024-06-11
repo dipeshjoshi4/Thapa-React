bc85df8382215f90b0a97643901234f87a6d03f0
dipeshjoshi <dipeshj2310@gmail.com>
2024-06-11 21:00:34 +0530

    add

--- react-thapa-first/src/components/SeriesCard.jsx
+++ react-thapa-first/src/components/SeriesCard.jsx
@@ -1,21 +1,32 @@
 export const SeriesCard = ({data}) => {
 
-    const { id, img_url, name, rating, description, cast, genre, watch_url } = data;
+    const { img_url, name, rating, description, cast, genre, watch_url } = data;
+
+    const btn_style = {
+        padding: "1.2rem 2.4rem",
+        border: "none",
+        fontSize: "1.6rem",
+        backgroundColor: "var(--btn-hover-bg-color)",
+        color: "var(--bg-color)"
+    }
+    
 
     return (
-        <li>
+        <li className="card">
             <div>
-                <img src={img_url} alt={name} width="60%" height="60%" />
+                <img src={img_url} alt={name} />
             </div>
 
-            <div>
+            <div className="card-content">
                 <h2>Name:{name}</h2>
-                <h3>Rating:{rating}</h3>
-                <p>{description}</p>
-                <h4>Genre:{genre}</h4>
-                <p>Cast:{cast}</p>
+
+                <h3 style={{fontSize:"1.5rem"}}>Rating:{rating}</h3>
+                <p>Summary:{description}</p>
+                <h4>Genre:{genre.join(" , ")}</h4>
+                <p>Cast:{cast.join(" , ")}</p>
+
                 <a href={watch_url} target="_blank">
-                    <button>Watch Now</button>
+                    <button style={btn_style}>Watch Now</button>
                 </a>
             </div>
         </li>
